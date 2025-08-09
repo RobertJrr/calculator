009d841b11bcc7aa2b8b9bb6194cae36e07f6945
Robert M <99871728+Robert010600@users.noreply.github.com>
2025-08-09 03:44:42 -0700

    Added some sanitizing methods to ensure better input functionality

--- script.js
+++ script.js
@@ -1,71 +1,24 @@
 //note: for in gets the property itself (so in array, use index returned to access element)
 // note: for of gets the actual value stored
 
-//handle input
-// function input(button,display){
-//     button.addEventListener("click",()=>{
-//         let text = button.value;
-//         if(beenReset){
-//             display.value = button.value;
-//             beenReset = false;
-//         }else{
-//             display.value += text;
-//         }  
-//     });
-// }
-
-// // handle clear
-// function clear(){
-//     display.value = "0";
-//     beenReset = true;
-//     finalCalculation = 0;
-// }
-
-// // operation functions
-// function sum(num){
-//     finalCalculation += num;
-// }
-
-// function subtract(num){
-//     finalCalculation -= num;
-// }
-
-// function mulitply(num){
-//     finalCalculation *= num;
-// }
-
-// function divide(num){
-//     finalCalculation /= num;
-// }
+//next steps:
+//get the total string and split based on +,*,/ and - signs
 
 
-// const buttons = document.querySelectorAll(".number-button, .operator-button, #decimal-button");
-// const display = document.querySelector("input");
-// const clearButton = document.querySelector("#clear-button");
-// const equalButton = document.querySelector("#equal-operator");
-
-// //operations array to hold all operations
-// let operations = [];
-// //if calculator has been reset (so it "changes" intial zero value and doesnt add to it)
-// let beenReset = true;
-
-// //final calculation
-// let finalCalculation = 0;
-
-// // to input numbers and operations to calculator display
-// buttons.forEach((button) =>{
-//    input(button,display);
-// });
-
-// // create the clear button
-// clearButton.addEventListener("click",clear);
-
-
+//Calculator class to hold all variables,functions and operations
+//to execute on calculator 
 class Calculator {
+    static OPERATORS = ["+", "-", "x", "/","."];
+
     constructor(buttons,display,clearButton,equalButton) {
+
         this.beenReset = true;
         this.finalCalculation = 0;
         this.operations = [];
+        
+        // keep track of operations
+        this.currentOperation = "";
+        this.lastOperation = "";
 
         // references to the nodes
         this.buttons = buttons;
@@ -86,6 +39,14 @@ class Calculator {
     }
     handleInput(button){
         button.addEventListener("click",()=>{
+
+            //to hold current operator value
+            this.currentOperation = button.value;
+            //if last input was operator and attempt to enter another, do not allow
+            if(this.wasOperator(this.lastOperation,this.currentOperation)){
+                return;
+            }
+
             let text = button.value;
             if(this.beenReset){
                 this.display.value = text;
@@ -93,6 +54,9 @@ class Calculator {
             }else{
                 this.display.value += text;
             }
+            // current operation now becomes last operation
+            this.lastOperation = this.currentOperation;
+
         });
     }
 
@@ -106,7 +70,14 @@ class Calculator {
         display.value = "0";
         this.finalCalculation = 0;
         this.beenReset = true;
+        this.lastOperation = "";
     }
+
+    wasOperator(lastValue,firstValue){
+        return Calculator.OPERATORS.includes(lastValue) && Calculator.OPERATORS.includes(firstValue);
+    }
+
+
 }
 
 function main(){
